fb44fa9088ddc48ad761a6ffaf160d22ef67618f
KITAKYU28 <bariumaka2012@gmail.com>
2013-09-04 13:37:17 +0900

    打撃結果を求める確率で、else　ifを用いて短縮した

--- yakyuu/ViewController.m
+++ yakyuu/ViewController.m
@@ -430,85 +430,85 @@
                 [self battingResultIs:homerun];
                         }
             
-            if(whatIsBattingResult >= 20 && whatIsBattingResult < 21){
+            else if(whatIsBattingResult < 21){
                 [self battingResultIs:threeBaseHit];
                         }
             
-            if(whatIsBattingResult >= 21 && whatIsBattingResult < 50) {
+            else if(whatIsBattingResult < 50) {
                 [self battingResultIs:twoBaseHit];
                         }
             
-            if (whatIsBattingResult >= 50 && whatIsBattingResult < 65) {
+            else if(whatIsBattingResult < 65) {
                 [self battingResultIs:singleHit];
                         }
             
-            if (whatIsBattingResult >= 65) {
+            else if (whatIsBattingResult >= 65) {
                 [self batterOut];
                         }
             break;
             
         case balance:
-            if (whatIsBattingResult < 10) {
+            if(whatIsBattingResult < 10) {
                 [self battingResultIs:homerun];
                     }
             
-            if (whatIsBattingResult >= 10 && whatIsBattingResult < 20) {
+            else if(whatIsBattingResult < 20) {
                 [self battingResultIs:threeBaseHit];
                         }
             
-            if (whatIsBattingResult >= 20 && whatIsBattingResult < 40) {
+            else if(whatIsBattingResult < 40) {
                 [self battingResultIs:twoBaseHit];
                         }
             
-            if (whatIsBattingResult >= 40 && whatIsBattingResult < 70) {
+            else if(whatIsBattingResult < 70) {
                 [self battingResultIs:singleHit];
                         }
             
-            if (whatIsBattingResult >= 70) {
+            else if(whatIsBattingResult >= 70) {
                 [self batterOut];
                         }
             break;
             
         case average:
-            if (whatIsBattingResult < 5) {
+            if(whatIsBattingResult < 5) {
                 [self battingResultIs:homerun];
                         }
             
-            if (whatIsBattingResult >= 5 && whatIsBattingResult < 15) {
+            else if(whatIsBattingResult < 15) {
                 [self battingResultIs:threeBaseHit];
                         }
             
-            if (whatIsBattingResult >= 15 && whatIsBattingResult < 35) {
+            else if(whatIsBattingResult < 35) {
                 [self battingResultIs:twoBaseHit];
             }
             
-            if (whatIsBattingResult >= 35 && whatIsBattingResult < 75) {
+            else if(whatIsBattingResult < 75) {
                 [self battingResultIs:singleHit];
                         }
             
-            if (whatIsBattingResult >= 75) {
+            else if(whatIsBattingResult >= 75) {
                 [self batterOut];
                         }
             break;
             
         case pinchHitter:
-            if (whatIsBattingResult < 75) {
+            if(whatIsBattingResult < 75) {
                 [self battingResultIs:homerun];
             }
             
-            if (whatIsBattingResult >= 75 && whatIsBattingResult < 79) {
+            else if(whatIsBattingResult < 79) {
                 [self battingResultIs:threeBaseHit];
             }
             
-            if (whatIsBattingResult >= 79 && whatIsBattingResult < 91) {
+            else if(whatIsBattingResult < 91) {
                 [self battingResultIs:twoBaseHit];
             }
             
-            if (whatIsBattingResult >= 91 && whatIsBattingResult < 95) {
+            else if(whatIsBattingResult < 95) {
                 [self battingResultIs:singleHit];
             }
             
-            if (whatIsBattingResult >= 95) {
+            else if(whatIsBattingResult >= 95) {
                 [self batterOut];
             }
             break;
